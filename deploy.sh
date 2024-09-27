d4463c7b798948cdbc9ebc8bdfdfdf69df67f501
shs1018 <1018gustj@naver.com>
2024-09-27 09:27:15 +0900

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# 인자로 커밋 메시지가 전달되지 않으면 기본 메시지 "update" 사용
+COMMIT_MESSAGE=${1:-"update"}
+
+# git 명령어 실행
 git add .
-git commit -m "minor update"
+git commit -m "$COMMIT_MESSAGE"
 git push
