bdaf5b689e4ec189345c9002663e42657760475c
shs1018 <1018gustj@naver.com>
2024-10-02 10:56:38 +0900

    test prepare update

---
+++ Python_basics/week2/20241004/answer_shs.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+'''
+문제 1
+개발환경설정 및 깃허브 push 전과정 서술
+'''
+
+'''
+그 외 문제는 실습 시 진행했던 코드에서 거의 판박이 수준. 
+colname이나 범위가 살짝 바뀌는 정도로 예상된다.
+판다스 슬라이싱/ 시각화
+
+오늘 - 내일 복습하다가 안되는 거 있으면 즉시 질문하기
+
+문제지는 프린트로 제공된다.
+
+output <- 시각화 이미지/ 전처리된 csv 저장하는 위치.
+이미지/ csv 저장하는 방법 복습하기.
+'''
+
+# data = pd.read_csv('C:/Users/1018g/OneDrive/바탕 화면/playground-series-s4e9/sample_submission.csv')
+
+# 데이터 프레임 내보내기
+txt = [1,2,3]
+print(txt)
